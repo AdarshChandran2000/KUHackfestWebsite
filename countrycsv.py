3171d9b43acc6099bc044c1c577999221ec10b82
Shaikh  Tahmidur <27241074+devtahmid@users.noreply.github.com>
2021-02-20 23:40:20 +0300

    Update countrycsv.py

--- countrycsv.py
+++ countrycsv.py
@@ -1,7 +1,7 @@
 import csv
 import pandas as pd
 df = pd.read_csv('coviddataset.csv', usecols=['Country_Region'])
-df_noDuplicates=df.drop_duplicates()
+df_noDuplicates=df.drop_duplicates() #remove duplicates
 print(df_noDuplicates.head(len(df_noDuplicates)))
-d_list = df_noDuplicates.to_dict()
+d_list = df_noDuplicates.to_dict()  #convert to list
 print(d_list)
